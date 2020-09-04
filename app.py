7a38f39054f35a96d09cecb928b898309577cc74
Chase.Gaddis <cgaddis36@gmail.com>
2020-09-04 14:17:45 -0600

    add routes for speech text-to-speech API to return string for audio

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
- 
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+@app.route('/speech/<phrase>', methods=['GET'])
+def speak(phrase):
+  audio = requests.get('https://api.voicerss.org/?key=5965c450ab324b21b297ea2d6ce96846&hl=en-us&src='+phrase+'&f=44khz_16bit_stereo&ssml=false&b64=true')
+
+  return audio.text
 
 if __name__ == "__main__":
     app.run(debug=True)
